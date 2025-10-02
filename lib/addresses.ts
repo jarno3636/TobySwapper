c9480b27a7fee7daa3cba3a606e9ba375b63a30e
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-10-02 07:47:31 -0500

    Update addresses.ts

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -11,7 +11,7 @@ export const TOBY     = "0xb8D98a102b0079B69FFbc760C8d857A31653e56e" as const;
 export const PATIENCE = "0x6D96f18F00B815B2109A3766E79F6A7aD7785624" as const;
 export const TABOSHI  = "0x3a1a33cf4553db61f0db2c1e1721cd480b02789f" as const;
 
-// Router / burn / your swapper
+// Router / burn / swapper
 export const ROUTER  = "0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24" as const;
 export const DEAD    = "0x000000000000000000000000000000000000dEaD" as const;
 export const SWAPPER = "0x6da391f470a00a206dded0f5fc0f144cae776d7c" as const;
