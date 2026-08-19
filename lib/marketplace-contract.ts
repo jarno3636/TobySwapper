dc476ae57646f880e15c342841b3c35b63183849
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2026-08-19 16:24:43 -0500

    Add files via upload

--- lib/marketplace-contract.ts
+++ lib/marketplace-contract.ts
@@ -53,6 +53,8 @@ export const ERC20_MARKET_ABI = [
 export const ERC721_MARKET_ABI = [
   { type: "function", name: "getApproved", stateMutability: "view", inputs: [{ name: "tokenId", type: "uint256" }], outputs: [{ name: "", type: "address" }] },
   { type: "function", name: "approve", stateMutability: "nonpayable", inputs: [{ name: "to", type: "address" }, { name: "tokenId", type: "uint256" }], outputs: [] },
+  { type: "function", name: "isApprovedForAll", stateMutability: "view", inputs: [{ name: "owner", type: "address" }, { name: "operator", type: "address" }], outputs: [{ name: "", type: "bool" }] },
+  { type: "function", name: "setApprovalForAll", stateMutability: "nonpayable", inputs: [{ name: "operator", type: "address" }, { name: "approved", type: "bool" }], outputs: [] },
 ] as const;
 
 export const ERC1155_MARKET_ABI = [
